4d700e27e8b5e6610c2f386a619b277a77aa31fb
Lapcuatui <macone-cw38zm@192.168.1.5>
2025-03-25 07:46:52 +0700

    done drap-drop-card-column

--- src/utils/formatters.js
+++ src/utils/formatters.js
@@ -1,6 +1,17 @@
 
+import _ from "lodash"
+
 export const capitalizeFirstLetter = (val) => {
   if (!val) return ''
   return `${val.charAt(0).toUpperCase()}${val.slice(1)}`
+}
+
+export const generatePlaceholderCard = (column) => {
+  return{
+    _id: `column-id-${column._id}-placeholder-card`,
+    boardId: column.boardId,
+    columnId: column._id,
+    FE_PlaceholderCard: true
+  }
 }
 
